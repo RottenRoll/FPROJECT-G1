c03ff177218a527b1594ffdb31ba21c39b8685e6
RottenRoll <nevs.sven.nevs@gmail.com>
2026-06-10 16:02:31 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -4,7 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const backgrounds = [
         'images/hero1.jpg',
         'images/hero2.jpg',
-        'images/hero3.jpg'
+        'images/hero3.jpg',
+        'images/hero4.jpg',
+        'images/hero5.jpg'
     ];
 
     let current = 0;
