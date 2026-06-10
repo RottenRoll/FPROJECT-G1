38dee0c1c2734a09ee93cb4f01bf27741e3a8433
RottenRoll <nevs.sven.nevs@gmail.com>
2026-06-10 15:18:38 +0800

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,3 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const hero = document.querySelector('.hero');
+    const backgrounds = [
+        'images/hero1.jpg',
+        'images/hero2.jpg',
+        'images/hero3.jpg'
+    ];
+
+    let current = 0;
+    hero.style.backgroundImage = `url('${backgrounds[current]}')`;
+    setInterval(() => {
+        current++;
+
+        if (current >= backgrounds.length) {
+            current = 0;
+        }
+
+        hero.style.backgroundImage =
+            `url('${backgrounds[current]}')`;
+
+    }, 4000);
+
+});
 .hero {
     background-color: var(--white);
     height: 60vh;
