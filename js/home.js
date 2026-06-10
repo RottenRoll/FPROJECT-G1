65477e5a8fec93836c42878e0768a21bfa191b9b
RottenRoll <nevs.sven.nevs@gmail.com>
2026-06-10 14:52:02 +0800

    Create home.js

---
+++ js/home.js
@@ -0,0 +1,19 @@
+.hero {
+    background-color: var(--white);
+    height: 60vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    border-bottom: 5px solid var(--primary-red);
+}
+
+.hero h1 {
+    font-size: 3rem;
+    margin-bottom: 10px;
+}
+
+.hero p {
+    font-size: 1.2rem;
+    margin-bottom: 20px;
+}
